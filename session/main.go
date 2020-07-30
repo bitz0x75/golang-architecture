98c5bed26f3262255be36bd94c1b2aa423481b10
Maxgit <maxwell@griffinkenya.com>
2020-07-30 14:20:51 +0300

    organize conyext into folders

--- session/main.go
+++ session/main.go
@@ -35,23 +35,3 @@ func GetAdmin(ctx context.Context) bool {
 	}
 	return false
 }
-
-// ctx := context.WithValue(context.Background(), "userID", 12345)
-// 	ctx = context.WithValue(ctx, 1, "admin")
-// 	if v := ctx.Value("userID"); v != nil {
-// 		fmt.Println(v)
-// 	} else {
-// 		fmt.Println("no value associated with that key")
-// 	}
-
-// 	if v := ctx.Value(1); v != nil {
-// 		fmt.Println(v)
-// 	} else {
-// 		fmt.Println("no value associated with that key")
-// 	}
-
-// 	if v := ctx.Value(2); v != nil {
-// 		fmt.Println(v)
-// 	} else {
-// 		fmt.Println("no value associated with that key")
-// 	}
